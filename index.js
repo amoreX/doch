13fbdd32ac2732c456340b87de9fc0699c609764
WahidX <wahidx93@gmail.com>
2024-01-12 19:36:18 +0530

    styling later

--- index.js
+++ index.js
@@ -32,9 +32,69 @@ app.get('/', (req, res) => {
     console.log("Website loaded succesfully");
 })
 
-app.get('/tech', (req, res) =>{
+app.get('/tech', async(req, res) =>{
     console.log('Getting tech');
-    res.render('tech.ejs',{"news1":"","news2":"","news3":"","news4":"","news5":"","news6":"","news7":"","news8":"","url1":"","url2":"","url3":"","url4":"","url5":"","url6":"","url7":"","url8":"","place":"What are you craving?"});
+    const news = await axios.get(`https://newsapi.org/v2/everything?q=bitcoin&pageSize=9&language=en&apiKey=49d58dd4dc4d4b11b4a20e32911bd048`);
+        console.log(news.data);
+
+        var newsdata = news.data;
+        var article1 = newsdata.articles[0].description;
+        var img1 = newsdata.articles[0].urlToImage;
+        var url1 = newsdata.articles[0].url;
+        var article2 = newsdata.articles[1].description;
+        var img2 = newsdata.articles[1].urlToImage;
+        var url2 = newsdata.articles[1].url;
+        var article3 = newsdata.articles[2].description;
+        var img3 = newsdata.articles[2].urlToImage;
+        var url3 = newsdata.articles[2].url;
+        var article4 = newsdata.articles[3].description;
+        var img4 = newsdata.articles[3].urlToImage;
+        var url4 = newsdata.articles[3].url;
+        var article5 = newsdata.articles[4].description;
+        var img5 = newsdata.articles[4].urlToImage;
+        var url5 = newsdata.articles[4].url;
+        var article6 = newsdata.articles[5].description;
+        var img6 = newsdata.articles[5].urlToImage;
+        var url6 = newsdata.articles[5].url;
+        var article7 = newsdata.articles[6].description;
+        var img7 = newsdata.articles[6].urlToImage;
+        var url7 = newsdata.articles[6].url;
+        var article8 = newsdata.articles[7].description;
+        var img8 = newsdata.articles[7].urlToImage;
+        var url8 = newsdata.articles[7].url;
+
+        console.log(newsdata);
+
+        res.render('tech.ejs', {
+            "news1": article1,
+            "news2": article2,
+            "news3": article3,
+            "news4":article4,
+            "news5": article5,
+            "news6": article6,
+            "news7": article7,
+            "news8":article8,
+            "img1": img1,
+            "img2": img2,
+            "img3": img3,
+            "img4": img4,
+            "img5": img5,
+            "img6": img6,
+            "img7": img7,
+            "img8": img8,
+            "url1": url1,
+            "url2": url2,
+            "url3": url3,
+            "url4": url4,
+            "url5": url5,
+            "url6": url6,
+            "url7": url7,
+            "url8": url8,
+            "place":"What are you craving?"
+        });
+        console.log(url1);
+    
+    // res.render('tech.ejs',{"news1":"","news2":"","news3":"","news4":"","news5":"","news6":"","news7":"","news8":"","url1":"","url2":"","url3":"","url4":"","url5":"","url6":"","url7":"","url8":"","place":"What are you craving?"});
 });
 
 app.post("/submit",async(req, res)=>{
